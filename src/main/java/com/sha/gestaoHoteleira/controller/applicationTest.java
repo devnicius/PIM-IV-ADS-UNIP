428d8bdfcac22d9eba0d8c163afedfcd15f9b86d
Vinícius Silva de Oliveira <devnicius@protonmail.com>
2021-09-08 13:05:19 -0300

    organizando estrutura

--- src/main/java/com/sha/gestaoHoteleira/controller/applicationTest.java
+++ src/main/java/com/sha/gestaoHoteleira/controller/applicationTest.java
@@ -1,11 +1,9 @@
-package com.sha.gestaoHoteleira.pages;
+package com.sha.gestaoHoteleira.controller;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.sha.gestaoHoteleira.controller.ContactController;
 
 @RestController
 public class applicationTest {
